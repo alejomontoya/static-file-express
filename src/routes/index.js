3861475a207350d25eb0809a8c7a1cf29645bf95
alejomontoya <montoyaa241@gmail.com>
2019-06-09 20:57:45 -0500

    change readme

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('index.html', { title: 'First website'});
+});
+
+router.get('/contact', (req, res) => {
+    res.render('contact.html', { title: 'Contact page'});
+});
+
+module.exports = router;
